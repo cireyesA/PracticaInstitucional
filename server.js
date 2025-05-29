e5e7febfc8c2d7f81028c8ef9ce9e083a6f97294
Lenid05 <astaizayasney@gmail.com>
2025-05-29 18:48:09 -0500

    cambios

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const cors = require("cors")
 
 
 const usuario = require('./routes/usuario');
